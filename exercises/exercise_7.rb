7a2dbac83cd5a7de470db4e336048f832837418b
Madeline Collier <madel@telus.net>
2017-08-01 22:01:56 +0000

    Finish exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Enter new store name"
+new_store_name = gets.chomp
+
+@store_new = Store.create name: new_store_name
+
+@store_new.errors.each do |attr, error|
+  puts "Error with #{attr}: #{error}"
+end
